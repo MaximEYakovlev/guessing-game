61611c32aa17e405b198d63c7db97f3efdde8be3
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2024-12-26 12:56:42 +0300

    the correct game finish has been implemented

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,7 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
         let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
@@ -25,7 +23,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
