84d111dc8233615d0053cfd202076e777703b73c
Maxim Yakovlev <it.trainee@yandex.ru>
2025-01-01 22:03:16 +0300

    added testing of the 'generate_random_number' function

--- src/random.rs
+++ src/random.rs
@@ -3,3 +3,17 @@ use rand::Rng;
 pub fn generate_random_number(range: std::ops::RangeInclusive<u32>) -> u32 {
     rand::thread_rng().gen_range(range)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::generate_random_number;
+
+    #[test]
+    fn test_generate_random_number_within_range() {
+        let range = 1..=10;
+        for _ in 0..100 {
+            let result = generate_random_number(range.clone());
+            assert!(range.contains(&result), "Result {} is not within range {:?}", result, range);
+        }
+    }
+}
