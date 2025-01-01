71706ad2f77e2a6f63398b583c87e374e10d9147
Maxim Yakovlev <it.trainee@yandex.ru>
2025-01-01 19:39:01 +0300

    defined the 'input' module

--- src/modules/input.rs
+++ src/modules/input.rs
@@ -1,20 +1,22 @@
 use std::io;
 
-pub trait InputHandler {
-    fn read(&self) -> Option<u32>;
-}
+pub mod input {
+    pub trait InputHandler {
+        fn read(&self) -> Option<u32>;
+    }
 
-pub struct ConsoleInput;
+    pub struct ConsoleInput;
 
-impl InputHandler for ConsoleInput {
-    fn read(&self) -> Option<u32> {
-        let mut input = String::new();
+    impl InputHandler for ConsoleInput {
+        fn read(&self) -> Option<u32> {
+            let mut input = String::new();
 
-        io::stdin().read_line(&mut input).ok()?;
+            io::stdin().read_line(&mut input).ok()?;
 
-        match input.trim().parse::<u32>() {
-            Ok(num) => Some(num),
-            Err(_) => None,
+            match input.trim().parse::<u32>() {
+                Ok(num) => Some(num),
+                Err(_) => None,
+            }
         }
     }
 }
