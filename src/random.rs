f8fdb3f0c2288a54f5f555d6ed026f5fd6e65081
Maxim Yakovlev <it.trainee@yandex.ru>
2025-01-07 12:29:00 +0300

    bugfixing: 'generate_random_number' function test

--- src/random.rs
+++ src/random.rs
@@ -15,13 +15,13 @@ impl RandomNumber for RandomGenerator {
 
 #[cfg(test)]
 mod tests {
-    use super::generate_random_number;
+    use super::{ RandomGenerator, RandomNumber };
 
     #[test]
     fn test_generate_random_number_within_range() {
         let range = 1..=10;
         for _ in 0..100 {
-            let result = generate_random_number(range.clone());
+            let result = RandomGenerator::generate_random_number(range.clone());
             assert!(range.contains(&result), "Result {} is not within range {:?}", result, range);
         }
     }
